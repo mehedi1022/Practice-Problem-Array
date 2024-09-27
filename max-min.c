473c4eec1baef2e049cafd72784de6babea5b4f7
mehedi1022 <mahdibinalamgir@gmail.com>
2024-09-27 22:26:45 +0600

    added max-min.c file

---
+++ max-min.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int num[100], n, i;
+
+
+    printf("How many numbers: ");
+    scanf("%d", &n);
+
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &num[i]);
+    }
+
+
+    int max = num[0];
+    int min = num[0];
+
+
+    for(i = 1; i < n; i++) {
+        if(max < num[i]){
+             max = num[i];
+        }
+
+        if(min > num[i]){
+            min = num[i];
+        }
+
+    }
+
+
+    printf("Maximum = %d\n", max);
+    printf("Minimum = %d\n", min);
+
+    return 0;
+}
